66674299f248c2186e0bfe7ddd83ca3d05cc1279
leonardo <d3pro3@outlook.com>
2021-12-20 18:14:30 -0300

    adequação das palavras para a resposta do exercicio

--- CursoJava/src/entities/Product.java
+++ CursoJava/src/entities/Product.java
@@ -4,20 +4,21 @@ public class Product {
 	public String name;
 	public double price;
 	public int quantity;
-	
-	
+
 	public double totalValueInStock() {
 		return this.price * this.quantity;
 	}
-	
+
 	public void addProducts(int quantity) {
 		this.quantity += quantity;
 	}
+
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
-	
+
 	public String toString() {
-		return "Product: " + this.name + ", Price: " + this.price + ", Quantity: " + this.quantity ;
+		return this.name + ", $" + String.format("%.2f", this.price) + "," + this.quantity + " units $" +String.format("%.2f", this.totalValueInStock());
+
 	}
 }
